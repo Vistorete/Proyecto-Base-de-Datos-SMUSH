df2e8c548fbf3f83a27d91dc50244badd23a2fc8
Victor Sanchez <victor.sanchez1@mail.udp.cl>
2019-06-29 17:52:58 -0500

    aaaaaaaaaa

--- database/run.sql
+++ database/run.sql
@@ -1,9 +1,8 @@
 --     \. c:/Proyecto BDD/BDD/database/run.sql
 
-
-
-\. c:/Proyecto BDD/BDD/database/db.sql
-\. c:/Proyecto BDD/BDD/database/users.sql
-\. c:/Proyecto BDD/BDD/database/equipos.sql
-\. c:/Proyecto BDD/BDD/database/torneo_local.sql
-\. c:/Proyecto BDD/BDD/database/personajes.sql
+--       \. c:/GitHub/Proyecto-Base-de-Datos-SMUSH/database/run.sql
+\. C:/GitHub/Proyecto-Base-de-Datos-SMUSH/database/db.sql
+\. C:/GitHub/Proyecto-Base-de-Datos-SMUSH/database/users.sql
+\. C:/GitHub/Proyecto-Base-de-Datos-SMUSH/database/equipos.sql
+\. C:/GitHub/Proyecto-Base-de-Datos-SMUSH/database/torneo_local.sql
+\. C:/GitHub/Proyecto-Base-de-Datos-SMUSH/database/personajes.sql
