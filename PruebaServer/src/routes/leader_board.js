7803cd09e60496ef2f75fddde12b134935908928
Matías Gutiérrez <matias.gutierrez1@mail.udp.cl>
2019-06-30 17:45:50 -0400

    leader

--- PruebaServer/src/routes/leader_board.js
+++ PruebaServer/src/routes/leader_board.js
@@ -13,16 +13,15 @@ var users;
 
 con.connect(function(err){
     if(err) throw err;
-    con.query("SELECT * FROM (SELECT users.id, users.username , (count(case WHEN partidas.score_jugador1 > partidas.score_jugador2 AND partidas.id_jugador1=users.id THEN 1 END) + count(case WHEN partidas.score_jugador1 < partidas.score_jugador2 AND partidas.id_jugador2=users.id THEN 1 END))/   count(case WHEN partidas.id_jugador1=users.id or partidas.id_jugador2=users.id THEN 1 END) * 100 AS winrate FROM partidas, users GROUP BY users.id ORDER BY winrate DESC) AS LEADER_BOARD WHERE LEADER_BOARD.winrate IS NOT NULL", function(err, result, fields){
+    con.query("SELECT *,(@i := @i + 1) as Rank FROM (SELECT users.id, users.username , (count(case WHEN partidas.score_jugador1 > partidas.score_jugador2 AND partidas.id_jugador1=users.id THEN 1 END) + count(case WHEN partidas.score_jugador1 < partidas.score_jugador2 AND partidas.id_jugador2=users.id THEN 1 END))/   count(case WHEN partidas.id_jugador1=users.id or partidas.id_jugador2=users.id THEN 1 END) * 100 AS winrate FROM partidas, users CROSS JOIN(SELECT @i := 0) r GROUP BY users.id ORDER BY winrate DESC) AS LEADER_BOARD WHERE LEADER_BOARD.winrate IS NOT NULL", function(err, result, fields){
         if(err) throw err;
         //console.log(result);
         leader_board=result;
     });
 });
 
-
 router.get('/', async (req, res) => {
-    res.render('links/leader_board',{leader_board});
+    res.render('links/leader_board', {leader_board});
     //const leader = await pool.query('SELECT * FROM (SELECT users.id, users.username , (count(case WHEN partidas.score_jugador1 > partidas.score_jugador2 AND partidas.id_jugador1=users.id THEN 1 END) +   count(case WHEN partidas.score_jugador1 < partidas.score_jugador2 AND partidas.id_jugador2=users.id THEN 1 END))/  count(case WHEN partidas.id_jugador1=users.id or partidas.id_jugador2=users.id THEN 1 END) * 100 AS winrate    FROM partidas, users   GROUP BY users.id ORDER BY winrate DESC) AS LEADER_BOARD WHERE LEADER_BOARD.winrate IS NOT NULL');
     //const users = await pool.query('SELECT * FROM users');
     console.log(leader_board);
