d9a645761bbd832b63b5da875206c1b16f2329f3
Victor Sanchez <victor.sanchez1@mail.udp.cl>
2019-06-30 14:36:48 -0500

    asdasdasd
    asdasd

--- PruebaServer/src/routes/leader_board.js
+++ PruebaServer/src/routes/leader_board.js
@@ -12,10 +12,10 @@ var con = mysql.createConnection({
 var users;
 con.connect(function(err){
     if(err) throw err;
-    con.query("SELECT * FROM partidas where partidas.id_jugador1=1001 or partidas.id_jugador2 = 1001", function(err, result, fields){
+    con.query("SELECT * FROM (SELECT users.id, users.username , (count(case WHEN partidas.score_jugador1 > partidas.score_jugador2 AND partidas.id_jugador1=users.id THEN 1 END) + count(case WHEN partidas.score_jugador1 < partidas.score_jugador2 AND partidas.id_jugador2=users.id THEN 1 END))/   count(case WHEN partidas.id_jugador1=users.id or partidas.id_jugador2=users.id THEN 1 END) * 100 AS winrate FROM partidas, users GROUP BY users.id ORDER BY winrate DESC) AS LEADER_BOARD WHERE LEADER_BOARD.winrate IS NOT NULL", function(err, result, fields){
         if(err) throw err;
         //console.log(result);
-        users=result;
+        leader_board=result;
     });
 });
 
@@ -24,7 +24,7 @@ router.get('/', async (req, res) => {
     res.render('links/leader_board');
     //const leader = await pool.query('SELECT * FROM (SELECT users.id, users.username , (count(case WHEN partidas.score_jugador1 > partidas.score_jugador2 AND partidas.id_jugador1=users.id THEN 1 END) +   count(case WHEN partidas.score_jugador1 < partidas.score_jugador2 AND partidas.id_jugador2=users.id THEN 1 END))/  count(case WHEN partidas.id_jugador1=users.id or partidas.id_jugador2=users.id THEN 1 END) * 100 AS winrate    FROM partidas, users   GROUP BY users.id ORDER BY winrate DESC) AS LEADER_BOARD WHERE LEADER_BOARD.winrate IS NOT NULL');
     //const users = await pool.query('SELECT * FROM users');
-    console.log(users);
+    console.log(leader_board);
 });
 
 router.post('/', async (req, res) => {
