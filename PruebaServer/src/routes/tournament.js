52458236ed8b6cfc15579cc4509d9ff0886af538
Victor Sanchez <victor.sanchez1@mail.udp.cl>
2019-06-30 20:34:59 -0500

    se pueden insertar torneos

--- PruebaServer/src/routes/tournament.js
+++ PruebaServer/src/routes/tournament.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 var mysql = require('mysql');
+//const pool = require('../database');
 
 var con = mysql.createConnection({
     host: "localhost",
@@ -15,16 +16,27 @@ var tournaments;
 
 con.connect(function(err){
     if(err) throw err;
+});
+
+router.get('/', async (req, res) => {
     con.query("SELECT * FROM torneos",function(err,result,fields){
         if(err) throw err;
         console.log(result);
         tournaments=result;
     });
-});
-
-router.get('/', async (req, res) => {
     res.render('links/tournament',{tournaments});
     console.log(tournaments);
+});
+router.post('/',async(req,res)=>{
+    const {id_organizador, nombre, id_local, fecha} = req.body;
+    const nuevoTorneo = {
+        id_organizador,
+        nombre,
+        id_local,
+        fecha
+    };
+    var query = await con.query("INSERT INTO torneos set ?",[nuevoTorneo]);
+    res.render('links/tournament',{tournaments});
 });
 
 
