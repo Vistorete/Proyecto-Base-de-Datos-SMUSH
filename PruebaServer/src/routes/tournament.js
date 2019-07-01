b2c467fe61c3feda9b79c4d8e27292d84b921628
Victor Sanchez <victor.sanchez1@mail.udp.cl>
2019-07-01 17:22:43 -0500

    t

--- PruebaServer/src/routes/tournament.js
+++ PruebaServer/src/routes/tournament.js
@@ -51,20 +51,16 @@ router.get('/delete/:id',async(req,res)=>{
     await res.redirect('/tournament');
 });
 
+
+var p_torneo;
 router.get('/info/:id',async(req,res)=>{
     const id_torneo = req.params.id;
-    await con.query('SELECT torneos.id_organizador, torneos.id, users.username, torneos.nombre, torneos.fecha, locales.direccion FROM users, torneos, locales WHERE users.id=torneos.id_organizador and locales.id=torneos.id_local and torneos.id = ?',[id_torneo],function(err,result,fields){
+    await con.query("SELECT p1.id_torneo, p1.id, p1.username as user1,p1.score_jugador1,p2.score_jugador2,  p2.username as user2, p1.num_ronda FROM (SELECT partidas.id, partidas.id_torneo, users.username, partidas.id_jugador1, partidas.score_jugador1, partidas.num_ronda    FROM users, partidas    WHERE users.id=partidas.id_jugador1) as p1,   (SELECT partidas.id, partidas.id_torneo, users.username, partidas.id_jugador2, partidas.score_jugador2    FROM users, partidas    WHERE users.id=partidas.id_jugador2) as p2 WHERE p1.id=p2.id AND p1.id_torneo = ?",[id_torneo],function(err,result,fields){
         if(err) throw err;
-        var torneo = result;
-        //console.log(result);
+        p_torneo = result;
+        console.log(result);
     });
-    await con.query("SELECT p1.id_torneo, p1.id, p1.username,p1.score_jugador1,p2.score_jugador2,  p2.username, p1.num_ronda FROM (SELECT partidas.id, partidas.id_torneo, users.username, partidas.id_jugador1, partidas.score_jugador1, partidas.num_ronda    FROM users, partidas    WHERE users.id=partidas.id_jugador1) as p1,   (SELECT partidas.id, partidas.id_torneo, users.username, partidas.id_jugador2, partidas.score_jugador2    FROM users, partidas    WHERE users.id=partidas.id_jugador2) as p2 WHERE p1.id=p2.id AND p1.id_torneo = ?",[id_torneo],function(err,result,fields){
-        if(err) throw err;
-        var p_torneo = result;
-        //console.log(result);
-    });
-    res.send('tula');
-    //res.render('links/t_info',{torneo: torneo, id_torneo: id});
+    res.render('links/t_info',{p_torneo});
 });
 
 module.exports = router; //Exporta el objeto router
