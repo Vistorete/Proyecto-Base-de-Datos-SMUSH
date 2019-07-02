31ef4527b70f2eb66b2b4cc7bccbb1d4d35dd975
Matías Gutiérrez <matias.gutierrez1@mail.udp.cl>
2019-07-02 12:33:20 -0400

    Agregar team y user

--- PruebaServer/src/routes/users.js
+++ PruebaServer/src/routes/users.js
@@ -11,35 +11,43 @@ var con = mysql.createConnection({
 });
 
 var users;
+var equipos;
 
 con.connect(function(err){
     if(err) throw err;
 });
 
 router.get('/', async (req, res) => {
-        await con.query('SELECT users.id, equipos.tag, users.username, users.fullname, users.edad, users.pais FROM users, equipos WHERE equipos.id = users.id_equipo', function(err,result,fields){
+        await con.query('SELECT * FROM users', function(err,result,fields){
         if(err) throw err;
         //console.log(result);
         users=result;
     });
 
     res.render('links/users', {users});
-    console.log(users);
+    //console.log(users);
     //res.send('listo');
 });
 
+/*router.get('/', async (req, res) => {
+    await con.query('SELECT * FROM equipos', function(err,result,fields){
+        if(err) throw err;
+        //console.log(result);
+        equipos=result;
+    });
+    res.render('links/users', {equipos});
+})*/
+
 router.post('/',async(req,res)=>{
-    const {id, tag, username, fullname, edad, pais} = req.body;
+    const { username, fullname, tag, edad, pais} = req.body;
     const newUser = {
-        id,
-        tag,
         username,
         fullname,
         edad,
         pais
     };
-    var query = await con.query("INSERT INTO users set ?",[newUser]);
-    res.render('/users',{users});
+    await con.query("INSERT INTO users set ?",[newUser]);
+    res.redirect('/users');
 });
 
 router.get('/:id', async (req, res) => {
@@ -55,15 +63,20 @@ router.get('/delete/:id', async (req, res) => { //Función para borrar usuario
     res.redirect('/users'); //Redirecciona a users.hbs
 });
 
+var users2;
+
 router.get('/edit/:id', async (req, res) => { //Función para obtener el usuario a editar
     const { id } = req.params;
-    const links = await con.query('SELECT users.username, users.fullname, users.edad, users.pais FROM users WHERE id = ?', [id]);
-    //console.log(links[0]);
-    res.render('links/user_edit', {links: links[0]});
+    await con.query('SELECT * FROM users WHERE id = ?', [id], function(err,result,fields){
+        if(err) throw err
+        users2 = result;
+    });
+    //console.log(users2);
+    res.render('links/user_edit', {users2});
 });
 
-router.get('/edit/:id', async (req, res) => { //Función para editar el usuario obtenido
-    const id = req.params.id;
+router.post('/edit/:id', async (req, res) => { //Función para editar el usuario obtenido
+    const { id } = req.params;
     const { username, fullname, edad, pais} = req.body;
     const newUser = {
         username,
@@ -71,9 +84,19 @@ router.get('/edit/:id', async (req, res) => { //Función para editar el usuario 
         edad,
         pais
     };
-    await pool.query('UPDATE users set ? WHERE id = ?', [newUser, id]);
+    await con.query('UPDATE users set ? WHERE id = ?', [newUser, id]);
     //res.send('hola');
     res.redirect('/users');
+});
+
+router.get('/profile/:id', async(req, res) => {
+    const { id } = req.params;
+    await con.query('SELECT * FROM users WHERE id = ?', [id], function(err,result,fields){
+        if(err) throw err
+        users2 = result;
+    });
+    //console.log(users2);
+    res.render('links/profile', {users2});
 });
 
 module.exports = router;
